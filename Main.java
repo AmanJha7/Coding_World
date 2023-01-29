bd568020f08a0f3901b3f53a869e8db0639f5a9c
Amanjha8503 <99260113+Amanjha8503@users.noreply.github.com>
2023-01-29 12:36:50 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,38 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        // Question says that print hello world text 5 times
-
-        // Question 2: print numbers 1 2 3 4 5 using recursive functions
-        fun(1);
-    }
-//    static void fun1(int n){
-//        System.out.println(n);
-//        fun2(2);
-//    }
-//
-//    static void fun2(int n){
-//        System.out.println(n);
-//        fun3(3);
-//    }
-//
-//    static void fun3(int n){
-//        System.out.println(n);
-//        fun4(4);
-//    }
-//    static void fun4(int n){
-//        System.out.println(n);
-//        fun5(5);
-//    }
-//    static void fun5(int n){
-//        System.out.println(n);
-//    }
-
-    static void fun(int n){
-        if (n == 5){
-            System.out.println(n);
+    public static void printPermutation(String str, String output){
+        if (str.length() == 0){
+            System.out.println("----> "+output+" "+str);
             return;
         }
-        System.out.println(n);
-        fun(n+1);
+
+        for (int i=0; i<str.length(); i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            System.out.println(str.substring(0,i)+str.substring(i+1));
+            printPermutation(newStr, output+currChar);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        printPermutation(str,"");
     }
 }
